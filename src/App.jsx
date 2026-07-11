df445c12ff3cba3c14d8a74dbe367adf7f852838
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-07-11 22:08:50 +0800

    feat(project): initialize memory card game skeleton with grid layout

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <header>
+          <div className="upper-container">
+            <h1>Memory Card Game</h1>
+            <div className="record">
+              <h2>Score: 0</h2>
+              <h2>Best Score: 0</h2>
+            </div>
+          </div>
+          <h3>
+            Get points by clicking on an image but don't click on any more than
+            once!
+          </h3>
+        </header>
+        <main>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+          <div>
+            <img></img>
+            <p>hi</p>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
